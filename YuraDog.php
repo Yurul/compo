5ef7ebce6ad32c1e05a4a71d7f00f2d89537a85a
Yurul <Yusyabox@gmail.com>
2017-01-22 11:39:30 +0200

    Add files via upload

---
+++ YuraDog.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Yurul;
+/**
+ * YuraDog is training with packagist
+ *
+ * @author Yurul
+ */
+class YuraDog {
+    public $voice="Wof!  Wof!";
+    public function obey(){
+        echo $this->voice;
+    }
+}
